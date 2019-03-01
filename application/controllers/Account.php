6fa629e86ad129898c81fbb88892770ce0853bc2
titoanugerah <titoanugerah@student.undip.ac.id>
2019-03-01 23:44:10 +0700

    Added login route, controler and model for account

---
+++ application/controllers/Account.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Account extends CI_Controller{
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->model('account_model');
+  }
+
+  public function login()
+  {
+    
+  }
+
+}
+
+ ?>
---
+++ application/models/account_model.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Account_model extends CI_model{
+
+  public function __construct()
+  {
+
+  }
+
+
+
+}
+
+ ?>
