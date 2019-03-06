5e429f965097d7acf01dcf9a23966a380e15bb2f
titoanugerah <titoanugerah@student.undip.ac.id>
2019-03-06 09:00:46 +0700

    Changed getSomeData into getSomeResult

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -96,7 +96,7 @@ class Admin_model extends CI_model{
      $this->db->insert('update_stock', $data);
   }
 
-  public function getSomeData($row, $id, $table)
+  public function getSomeResult($row, $id, $table)
   {
     $where = array($row => $id);
     $query = $this->db->get_where($table, $where);
